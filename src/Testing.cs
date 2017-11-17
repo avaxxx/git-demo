c5a78fc5385ec05ab6a527a728926e79d6ca83a0
avaxxx <avaxxx@gmail.com>
2017-11-17 22:22:27 +0100

    Remove conflict marker

--- src/Testing.cs
+++ src/Testing.cs
@@ -1,9 +1,6 @@
 public class Testing
 {
-<<<<<<< HEAD
     public string Framework { get; set; }
-=======
     public string Enviroment { get; set; }
     public string Location { get; set; }
->>>>>>> Testing
 }
