42b834e1b1e21b3e115b857a9f1f286752ecfe1c
avaxxx <avaxxx@gmail.com>
2017-11-17 21:47:20 +0100

    Checkin 7

--- src/Person.cs
+++ src/Person.cs
@@ -2,4 +2,6 @@ public class Person
 {
     public string FirstName { get; set; }
     public string Lastname { get; set; }
+    public string EmailAddress { get; set; }
+    public string Password { get; set; }
 }
